2ce8a6ad70c85729be839e019e5d147b3eae721f
diegoarnanz-maker <diegoarnanzlozano96@gmail.com>
2025-02-01 18:05:39 +0100

    Proyecto inicializado

---
+++ src/test/java/Actividad_4_ALD/Actividad4AldApplicationTests.java
@@ -0,0 +1,13 @@
+package Actividad_4_ALD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad4AldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
